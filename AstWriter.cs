449757f1ff00950d08a963f96e9aa4dc60916490
Christoffer <clindow@gmail.com>
2022-11-27 11:02:10 +0700

    Comment the AstWriter for now

--- AstWriter.cs
+++ AstWriter.cs
@@ -1,49 +1,49 @@
-using System.Collections.Generic;
-using System.Text;
+// using System.Collections.Generic;
+// using System.Text;
 
-namespace App
-{
+// namespace App
+// {
 
-    public class AstPrinter : Expr.Visitor<string>
-    {
-        public string visitBinaryExpr(Expr.Binary expr)
-        {
-            return Parenthesize(expr.op.Lexeme,
-                                new List<Expr> { expr.Left, expr.Right });
-        }
+//     public class AstPrinter : Expr.Visitor<string>
+//     {
+//         public string visitBinaryExpr(Expr.Binary expr)
+//         {
+//             return Parenthesize(expr.op.Lexeme,
+//                                 new List<Expr> { expr.Left, expr.Right });
+//         }
 
-        public string visitGroupingExpr(Expr.Grouping expr)
-        {
-            return Parenthesize("group", new List<Expr> { expr.Expression });
-        }
+//         public string visitGroupingExpr(Expr.Grouping expr)
+//         {
+//             return Parenthesize("group", new List<Expr> { expr.Expression });
+//         }
 
-        public string visitLiteralExpr(Expr.Literal expr)
-        {
-            if (expr.Value == null) return "nil";
-            return expr.Value.ToString();
-        }
+//         public string visitLiteralExpr(Expr.Literal expr)
+//         {
+//             if (expr.Value == null) return "nil";
+//             return expr.Value.ToString();
+//         }
 
-        public string visitUnaryExpr(Expr.Unary expr)
-        {
-            return Parenthesize(expr.Op.Lexeme, new List<Expr> { expr.Right });
-        }
+//         public string visitUnaryExpr(Expr.Unary expr)
+//         {
+//             return Parenthesize(expr.Op.Lexeme, new List<Expr> { expr.Right });
+//         }
 
-        public string Print(Expr expr)
-        {
-            return expr.accept(this);
-        }
+//         public string Print(Expr expr)
+//         {
+//             return expr.accept(this);
+//         }
 
-        private string Parenthesize(string name, List<Expr> exprs)
-        {
-            StringBuilder builder = new();
-            builder.Append("(").Append(name);
-            exprs.ForEach(expr =>
-            {
-                builder.Append(" ");
-                builder.Append(expr.accept(this));
-            });
-            builder.Append(")");
-            return builder.ToString();
-        }
-    }
-}
+//         private string Parenthesize(string name, List<Expr> exprs)
+//         {
+//             StringBuilder builder = new();
+//             builder.Append("(").Append(name);
+//             exprs.ForEach(expr =>
+//             {
+//                 builder.Append(" ");
+//                 builder.Append(expr.accept(this));
+//             });
+//             builder.Append(")");
+//             return builder.ToString();
+//         }
+//     }
+// }
