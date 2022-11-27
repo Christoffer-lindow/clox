dd031a04715c2dd4e0430a251fe1b0750e51e7f9
Christoffer <clindow@gmail.com>
2022-11-27 11:03:57 +0700

    Add new Stmt classes

--- Stmt.cs
+++ Stmt.cs
@@ -3,9 +3,21 @@ namespace App {
 	public abstract class Stmt {
 		public abstract T accept<T>(Visitor<T> visitor);
 	public interface Visitor<T> {
+		T visitBlockStmt (Block stmt );
 		T visitExpressionStmt (Expression stmt );
 		T visitPrintStmt (Print stmt );
+		T visitVarStmt (Var stmt );
 	}
+	public class Block: Stmt{
+		public List<Stmt> Statements;
+		public Block(List<Stmt> Statements) {
+			this.Statements = Statements;
+		}
+		public override T accept<T>(Visitor<T> visitor) {
+			 return visitor.visitBlockStmt(this);
+		}
+	}
+
 	public class Expression: Stmt{
 		public Expr expression;
 		public Expression(Expr expression) {
@@ -23,6 +35,18 @@ namespace App {
 		}
 		public override T accept<T>(Visitor<T> visitor) {
 			 return visitor.visitPrintStmt(this);
+		}
+	}
+
+	public class Var: Stmt{
+		public Token Name;
+		public Expr Initializer;
+		public Var(Token Name, Expr Initializer) {
+			this.Name = Name;
+			this.Initializer = Initializer;
+		}
+		public override T accept<T>(Visitor<T> visitor) {
+			 return visitor.visitVarStmt(this);
 		}
 	}
 
